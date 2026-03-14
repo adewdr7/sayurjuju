2b210eaa48518e0875efdbde3e7eb5060d0b0600
adewdr7 <adewdr7@gmail.com>
2026-03-14 15:10:20 +0700

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBWkPqyh54IRccNz2YjuB1bL5r49YadAP4",
+    authDomain: "sayurjuju-bfe82.firebaseapp.com",
+    projectId: "sayurjuju-bfe82",
+    storageBucket: "sayurjuju-bfe82.firebasestorage.app",
+    messagingSenderId: "754413439458",
+    appId: "1:754413439458:web:341c9fe99fd9b1550551bb"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    const { title, body } = payload.notification;
+    self.registration.showNotification(title, { body });
+});
